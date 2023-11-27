5c0592f719c1fefba02757f052432a0b3004afd7
charlie-killian <91706357+charlie-killian@users.noreply.github.com>
2023-11-27 16:41:39 -0600

    clc

--- final_project.m
+++ final_project.m
@@ -1,3 +1,7 @@
+clc
+close all
+clear all
+
 % RGB -> HSI
 % figure
 logs = imread('woodlogs_b.png');
@@ -12,6 +16,7 @@ h = h < .10;
 
 h = bwareaopen(h, 10000);
 
+
 h = imfill(h, "holes");
 h = uint8(h);
 imshow(h, [])
@@ -19,6 +24,7 @@ imshow(h, [])
 s = hsv_logs(:,:,2);
 
 s = s < 0.6;
+% s = bwareafilt(s, [150000 200000]);
 s = uint8(s);
 
 figure
@@ -31,8 +37,16 @@ imshow(logs_bw, [])
 
 edge_img = edge(logs_bw, "canny", [0.6, 0.9], 1.5); 
 
+% [himage, t, r] = hough(edge_img, "Theta", 1, "RhoResolution", 1);
+% 
+% [himage, r] = imfindcircles(edge_img, [100 200]);
+% viscircles(himage, r);
+
 figure
 imshow(edge_img, [])
+
+figure
+% imshow(himage, [])
 
 
 

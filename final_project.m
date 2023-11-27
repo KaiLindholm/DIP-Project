01de52b69a45b9a9d917b38f61f5967addc4ba64
KaiLindholm <kailindholm20022@gmail.com>
2023-11-27 17:42:56 -0600

    example circle hough from internet

--- final_project.m
+++ final_project.m
@@ -1,11 +1,8 @@
-clc
 close all
-clear all
 
 % RGB -> HSI
 % figure
 logs = imread('woodlogs_b.png');
-
 
 hsv_logs = rgb2hsv(logs);
 % imshow(hsv_logs, [])
@@ -41,45 +38,50 @@ edge_img = edge(logs_bw, "canny", [0.6, 0.9], 1.5);
 % 
 % [himage, r] = imfindcircles(edge_img, [100 200]);
 % viscircles(himage, r);
-
 figure
+subplot(1,2,1); 
+imshow(logs_bw, [])
+subplot(1,2,2); 
 imshow(edge_img, [])
-figure, 
-imshow(logs_bw - h, [])
 %%
-i
+
+[acc, T, R] = hough(edge_img, 'ThetaResolution', 0.75, 'RhoResolution', 0.75);
 figure, 
-[acc,T,R] = hough(edge_img, 'ThetaResolution', 0.75, 'RhoResolution', 0.75);
 subplot(1,2,1)
 imshow(log(acc+1), [], 'XData', T, 'YData', R, 'InitialMagnification','fit');
 xlabel('\theta'), ylabel('\rho'); axis on, axis normal;
-
-<<<<<<< Updated upstream
-figure
 % imshow(himage, [])
-
-
-
-
-=======
 P = houghpeaks(acc, 15, 'Threshold', 0.25*max(acc(:)) );
-    
+  
+
+figure, 
+subplot(1,2,1); 
+radius = 105; 
+[y0detect,x0detect,acc] = circle_hough(edge_img, radius, 4);
+imshow(log(acc+1), [])
+
+% subplot(1,2,2); 
+% imshow(logs, []);
+% hold on
+% drawcircle('Center',[y0detect, x0detect], 'Radius',radius)
+
+
+
 %%
-figure
-a = imread("circlesBrightDark.png");
-imshow(a)
-a = a < 100;
->>>>>>> Stashed changes
-
-stats = regionprops('table',a,'Centroid', ...
-                             'MajorAxisLength','MinorAxisLength');
-
-% Get centers and radii of the circles
-centers = stats.Centroid;
-diameters = mean([stats.MajorAxisLength stats.MinorAxisLength],2);
-radii = diameters/2;
-
-% Plot the circles
-hold on
-viscircles(centers,radii);
-hold off
+% figure
+% a = imread("circlesBrightDark.png");
+% imshow(edge_img, [])
+% a = a < 100;
+% 
+% stats = regionprops('table',edge_img,'Centroid', ...
+%                              'MajorAxisLength','MinorAxisLength');
+% 
+% % Get centers and radii of the circles
+% centers = stats.Centroid;
+% diameters = mean([stats.MajorAxisLength stats.MinorAxisLength],2);
+% radii = diameters/2;
+% 
+% % Plot the circles
+% hold on
+% viscircles(centers,radii);
+% hold off

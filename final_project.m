bd24d29058e63c0608c17a8012111a66e4e4db10
charlie-killian <91706357+charlie-killian@users.noreply.github.com>
2023-11-28 12:48:48 -0600

    sensitivity 0.97

--- final_project.m
+++ final_project.m
@@ -1,6 +1,6 @@
 clc
 close all
-clear all
+clear
 
 % RGB -> HSI
 % figure
@@ -19,7 +19,7 @@ h = bwareaopen(h, 10000);
 
 h = imfill(h, "holes");
 h = uint8(h);
-imshow(h, [])
+imshow(h, []);
 
 s = hsv_logs(:,:,2);
 
@@ -32,54 +32,60 @@ imshow(s, [])
 
 logs_bw = h .* s;
 
-figure
-imshow(logs_bw, [])
 
 edge_img = edge(logs_bw, "canny", [0.6, 0.9], 1.5); 
 
 % [himage, t, r] = hough(edge_img, "Theta", 1, "RhoResolution", 1);
-% 
-% [himage, r] = imfindcircles(edge_img, [100 200]);
-% viscircles(himage, r);
+%%
+
+
+[c, r] = imfindcircles(edge_img, [100 250], ObjectPolarity="bright", Sensitivity=0.97);
+
+figure
+imshow(logs_bw, [])
+viscircles(c, r);
+
 
 figure
 imshow(edge_img, [])
-figure, 
-imshow(logs_bw - h, [])
-%%
-i
-figure, 
-[acc,T,R] = hough(edge_img, 'ThetaResolution', 0.75, 'RhoResolution', 0.75);
-subplot(1,2,1)
-imshow(log(acc+1), [], 'XData', T, 'YData', R, 'InitialMagnification','fit');
-xlabel('\theta'), ylabel('\rho'); axis on, axis normal;
+viscircles(c, r);
 
-<<<<<<< Updated upstream
-figure
+
+%%
+
+% figure, 
+% imshow(logs_bw - h, [])
+% %%
+% i
+% figure, 
+% [acc,T,R] = hough(edge_img, 'ThetaResolution', 0.75, 'RhoResolution', 0.75);
+% subplot(1,2,1)
+% imshow(log(acc+1), [], 'XData', T, 'YData', R, 'InitialMagnification','fit');
+% xlabel('\theta'), ylabel('\rho'); axis on, axis normal;
+
+% figure
 % imshow(himage, [])
 
 
 
 
-=======
-P = houghpeaks(acc, 15, 'Threshold', 0.25*max(acc(:)) );
+% P = houghpeaks(acc, 15, 'Threshold', 0.25*max(acc(:)) );
     
 %%
-figure
-a = imread("circlesBrightDark.png");
-imshow(a)
-a = a < 100;
->>>>>>> Stashed changes
-
-stats = regionprops('table',a,'Centroid', ...
-                             'MajorAxisLength','MinorAxisLength');
-
-% Get centers and radii of the circles
-centers = stats.Centroid;
-diameters = mean([stats.MajorAxisLength stats.MinorAxisLength],2);
-radii = diameters/2;
-
-% Plot the circles
-hold on
-viscircles(centers,radii);
-hold off
+% figure
+% a = imread("circlesBrightDark.png");
+% imshow(a)
+% a = a < 100;
+% 
+% stats = regionprops('table',a,'Centroid', ...
+%                              'MajorAxisLength','MinorAxisLength');
+% 
+% % Get centers and radii of the circles
+% centers = stats.Centroid;
+% diameters = mean([stats.MajorAxisLength stats.MinorAxisLength],2);
+% radii = diameters/2;
+% 
+% % Plot the circles
+% hold on
+% viscircles(centers,radii);
+% hold off

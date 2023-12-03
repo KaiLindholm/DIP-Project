429f1da5a958acf94cb27fb25c07e9ed4de41038
charlie-killian <91706357+charlie-killian@users.noreply.github.com>
2023-12-02 22:02:09 -0600

    medfilt2

--- final_project_2.m
+++ final_project_2.m
@@ -1,14 +1,40 @@
 img = imread("ptag_b.png");
 
-img_hsv = rgb2hsv(img);
-
-h = img_hsv(:,:,1);
-s = img_hsv(:,:,2);
-v = img_hsv(:,:,3);
+img = rgb2gray(img);
 
 figure(1)
-subplot(1,3,1); imshow(h);
-subplot(1,3,2); imshow(s);
-subplot(1,3,3); imshow(v);
 
-s = s < 0.10;
+imshow(img);
+impixelinfo
+
+figure(2)
+histeq(img);
+imhist(img);
+
+figure(3)
+hi_img = img > 230 & img < 248;
+mid_img = img > 180 & img < 220;
+
+% hi_img = uint8(hi_img);
+% hi_img = hi_img .* img;
+
+% h = fspecial("gaussian", [5 5], 10);
+% hi_img = imfilter(hi_img, h, "same", "replicate");
+
+
+hi_img = medfilt2(hi_img, [5 10]);
+hi_img = bwareaopen(hi_img, 1000, 4);
+
+imshow(hi_img);
+
+
+
+% figure(4)
+% mid_img = uint8(mid_img);
+% mid_img = mid_img .* img;
+% imshow(mid_img)
+% 
+% figure(5)
+% ptag = hi_img + mid_img;
+% 
+% imshow(ptag)
